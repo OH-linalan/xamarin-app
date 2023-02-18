06ae9fafe00708c038d1b2ba9eb1012aedd7a3d3
Hyunmyung Oh <90341832+OH-linalan@users.noreply.github.com>
2023-02-18 05:30:39 +0000

    cs hello

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace console1{
+    class programs{
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+        }
+    }
+}
